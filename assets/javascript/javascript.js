ba1ae32fabad41b091ca15b8c718fa1f03136b4d
Tim Andersson <timalexanderandersson@hotmail.com>
2024-04-26 06:13:39 +0000

    Added all elements in to the javascript

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+let RockHere = document.getElementById('RockHere');
+let PaperHere = document.getElementById('PaperHere');
+let SiccorsHere = document.getElementById('ScissorsHere');
+let thebiggamearea = document.getElementById('thebiggamearea');
+let TheGameHere = document.getElementById('Rockpaperandscissors');
+let ScorePartOfGame = document.getElementById('scorepart')
+
+
+
