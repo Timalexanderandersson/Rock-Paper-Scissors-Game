a2e1def0dfc303e92e379a63586ba0ba6fa84b8b
Tim Andersson <timalexanderandersson@hotmail.com>
2024-04-26 10:53:16 +0000

    added everything in startthegame function and styled everything and created new elements and css class to edit the startthegame

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,7 +4,9 @@ let SiccorsHere = document.getElementById('ScissorsHere');
 let thebiggamearea = document.getElementById('thebiggamearea');
 let TheGameHere = document.getElementById('Rockpaperandscissors');
 let ScorePartOfGame = document.getElementById('scorepart');
-let AlternativesInGameDiv = document.getElementById('AlternativsForGame')
+let AlternativesInGameDiv = document.getElementById('AlternativsForGame');
+let headerhere = document.getElementById('Headerhere');
+let mainpartHere = document.getElementById('mainPart')
 
 
 let ComputerScoreHere = 0;
@@ -17,15 +19,20 @@ function startTheGame() {
     TheGameHere.style.backgroundColor = '#A78387';
     AlternativesInGameDiv.innerHTML = '';
     ScorePartOfGame.innerHTML = '';
-    ScorePartOfGame.style.borderStyle = 'none';
-    thebiggamearea.style.backgroundColor = '#A78387';
     ScorePartOfGame.style.borderStyle = 'none'
-   
-    let MyStartButton = document.createElement('button')
+    headerhere.innerText = '';
+    thebiggamearea.style.backgroundColor = '#A78387';
+    thebiggamearea.style.borderColor = '#dbccce';
+    thebiggamearea.style.borderStyle = 'solid';
 
+    let MyNewHeader = document.createElement('h1')
+    MyNewHeader.innerText = 'You ready to Play Rock Paper and Sicssors?'
+    let MyStartButton = document.createElement('button')
+    TheGameHere.appendChild(MyNewHeader)
     MyStartButton.innerText = 'Start Game!'
     TheGameHere.appendChild(MyStartButton)
     MyStartButton.classList.add('StartButton')
+    MyNewHeader.classList.add('TheNewHeader')
 
 };
 startTheGame()
