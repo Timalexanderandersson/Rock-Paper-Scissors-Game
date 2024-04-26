781ea3dfde3cd4f2bba56901614adbf735bfc33e
Tim Andersson <timalexanderandersson@hotmail.com>
2024-04-26 09:37:42 +0000

    stle everything in startthegame function to be nothing on the first site

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,7 +3,23 @@ let PaperHere = document.getElementById('PaperHere');
 let SiccorsHere = document.getElementById('ScissorsHere');
 let thebiggamearea = document.getElementById('thebiggamearea');
 let TheGameHere = document.getElementById('Rockpaperandscissors');
-let ScorePartOfGame = document.getElementById('scorepart')
+let ScorePartOfGame = document.getElementById('scorepart');
+let AlternativesInGameDiv = document.getElementById('AlternativsForGame')
 
 
+let ComputerScoreHere = 0;
+let PlayerScore = 0;
 
+
+//Function to start the game
+function startTheGame() {
+    TheGameHere.style.borderStyle = 'none'
+    TheGameHere.style.backgroundColor = '#A78387';
+    AlternativesInGameDiv.innerHTML = '';
+    ScorePartOfGame.innerHTML = '';
+    ScorePartOfGame.style.borderStyle = 'none';
+    thebiggamearea.style.backgroundColor = '#A78387';
+    ScorePartOfGame.style.borderStyle = 'none'
+
+}
+startTheGame()
