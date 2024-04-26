f554b5e5b97daade76d48b0252143ad171e9d523
Tim Andersson <timalexanderandersson@hotmail.com>
2024-04-26 11:07:34 +0000

    deleted everything inside startTheGame function

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,6 +7,7 @@ let ScorePartOfGame = document.getElementById('scorepart');
 let AlternativesInGameDiv = document.getElementById('AlternativsForGame');
 let headerhere = document.getElementById('Headerhere');
 let mainpartHere = document.getElementById('mainPart')
+let MyStartButton = document.createElement('button')
 
 
 let ComputerScoreHere = 0;
@@ -15,24 +16,5 @@ let PlayerScore = 0;
 
 //Function to start the game
 function startTheGame() {
-    TheGameHere.style.borderStyle = 'none'
-    TheGameHere.style.backgroundColor = '#A78387';
-    AlternativesInGameDiv.innerHTML = '';
-    ScorePartOfGame.innerHTML = '';
-    ScorePartOfGame.style.borderStyle = 'none'
-    headerhere.innerText = '';
-    thebiggamearea.style.backgroundColor = '#A78387';
-    thebiggamearea.style.borderColor = '#dbccce';
-    thebiggamearea.style.borderStyle = 'solid';
-
-    let MyNewHeader = document.createElement('h1')
-    MyNewHeader.innerText = 'You ready to Play Rock Paper and Sicssors?'
-    let MyStartButton = document.createElement('button')
-    TheGameHere.appendChild(MyNewHeader)
-    MyStartButton.innerText = 'Start Game!'
-    TheGameHere.appendChild(MyStartButton)
-    MyStartButton.classList.add('StartButton')
-    MyNewHeader.classList.add('TheNewHeader')
 
 };
-startTheGame()
