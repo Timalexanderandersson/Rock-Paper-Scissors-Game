02d1326be064b1704530aee36cbaef772a909159
Tim Andersson <timalexanderandersson@hotmail.com>
2024-05-02 08:39:42 +0000

    Clean up the format in the code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -28,66 +28,66 @@ ScorePartOfGame.appendChild(TextForComputer);
 
 // Game Text 
 let GameareaText = document.createElement('h3');
-    GameareaText.innerText = 'Choose your alternativ!';
-    TheGameHere.appendChild(GameareaText);
-    GameareaText.classList.add('TextInsideGame');
-    let bestOFthree = document.createElement('p')
-    bestOFthree.innerText = 'First To 3 Scores Wins!'
-    TheGameHere.appendChild(bestOFthree)
+GameareaText.innerText = 'Choose your alternativ!';
+TheGameHere.appendChild(GameareaText);
+GameareaText.classList.add('TextInsideGame');
+let bestOFthree = document.createElement('p')
+bestOFthree.innerText = 'First To 3 Scores Wins!'
+TheGameHere.appendChild(bestOFthree)
 
 
 // Alternativs For Game
-let ChoicesHere = ["Rock","Paper","Scissors"];
+let ChoicesHere = ["Rock", "Paper", "Scissors"];
 
 
 
 //Function to start the game
 function playingTheGame(AlternativHere) {
-    
 
 
-// computer choices
 
-    let theComputerNumber = Math.floor(Math.random()*3);
+    // computer choices
+
+    let theComputerNumber = Math.floor(Math.random() * 3);
     let RandomChoicesHere = ChoicesHere[theComputerNumber];
     console.log(RandomChoicesHere);
 
 
 
-    if(AlternativHere === RandomChoicesHere){
+    if (AlternativHere === RandomChoicesHere) {
         GameareaText.innerText = 'Same As The Computer!';
     } else if (AlternativHere === "Rock" && RandomChoicesHere === 'Scissors') {
-       
+
         GameareaText.innerText = 'You Won!';
         ++PlayersScoreHere;
         textForThePlayer.textContent = 'You win :' + PlayersScoreHere;
 
-    } else if (AlternativHere === 'Paper' && RandomChoicesHere === "Rock"){
+    } else if (AlternativHere === 'Paper' && RandomChoicesHere === "Rock") {
         GameareaText.innerText = 'You Won!';
         ++PlayersScoreHere;
         textForThePlayer.textContent = 'You win :' + PlayersScoreHere;
 
-    } else if (AlternativHere === "Scissors" && RandomChoicesHere === "Paper"){
+    } else if (AlternativHere === "Scissors" && RandomChoicesHere === "Paper") {
         GameareaText.innerText = 'You Won!';
         ++PlayersScoreHere;
-        textForThePlayer.textContent = 'You win :'+ PlayersScoreHere;
+        textForThePlayer.textContent = 'You win :' + PlayersScoreHere;
     };
 
     // Computer Wins
-    if(RandomChoicesHere === "Rock" && AlternativHere === "Scissors"){
+    if (RandomChoicesHere === "Rock" && AlternativHere === "Scissors") {
         GameareaText.innerText = 'You Lose!';
         ++ComputerScoreHere;
-        TextForComputer.textContent = 'Computer win :'+ ComputerScoreHere;
+        TextForComputer.textContent = 'Computer win :' + ComputerScoreHere;
 
-    } else if(RandomChoicesHere === "Paper" && AlternativHere === "Rock"){
+    } else if (RandomChoicesHere === "Paper" && AlternativHere === "Rock") {
         GameareaText.innerText = 'You Lose!';
         ++ComputerScoreHere;
-        TextForComputer.textContent = 'Computer win :'+ ComputerScoreHere;
+        TextForComputer.textContent = 'Computer win :' + ComputerScoreHere;
 
-    } else if (RandomChoicesHere === "Scissors" && AlternativHere === "Paper"){
+    } else if (RandomChoicesHere === "Scissors" && AlternativHere === "Paper") {
         GameareaText.innerText = 'You Lose!';
         ++ComputerScoreHere;
-        TextForComputer.textContent = 'Computer win :'+ ComputerScoreHere;
+        TextForComputer.textContent = 'Computer win :' + ComputerScoreHere;
     };
     CountingDownGame()
     console.log('PlayingtheGamehere');
@@ -97,30 +97,30 @@ function playingTheGame(AlternativHere) {
 
 
 //EventListeners for the buttons
-RockHere.addEventListener('click', function (){
+RockHere.addEventListener('click', function () {
     playingTheGame("Rock")
 });
 
-PaperHere.addEventListener('click', function (){
+PaperHere.addEventListener('click', function () {
     playingTheGame("Paper")
 });
-SiccorsHere.addEventListener('click', function (){
+SiccorsHere.addEventListener('click', function () {
     playingTheGame("Scissors")
 });
 
 
 // function for ending game if its over 3 wins
-function CountingDownGame(){
-    if(PlayersScoreHere >= 3){
+function CountingDownGame() {
+    if (PlayersScoreHere >= 3) {
         GameareaText.innerText = 'congratulations you won!';
         GameareaText.style.color = 'Green'
         GameareaText.style.fontSize = '1.3rem'
         AlternativesInGameDiv.style.display = 'none';
-        ScorePartOfGame.style.display= 'none';
+        ScorePartOfGame.style.display = 'none';
         bestOFthree.style.display = 'none';
         GameareaText.appendChild(PlayAgainHere)
 
-    } else if(ComputerScoreHere >= 3) {
+    } else if (ComputerScoreHere >= 3) {
         GameareaText.innerText = 'Sorry You lose!';
         GameareaText.style.color = 'Red'
         GameareaText.style.fontSize = '1.5rem'
@@ -129,17 +129,17 @@ function CountingDownGame(){
         bestOFthree.style.display = 'none';
         GameareaText.appendChild(PlayAgainHere)
 
-        
+
     }
 };
 
 
 
 // Restart Game
-function StartGameAgain(){
+function StartGameAgain() {
     location.reload()
     console.log('restart')
 };
 
 //BUtton for Restart The Game
-PlayAgainHere.addEventListener('click',StartGameAgain)
+PlayAgainHere.addEventListener('click', StartGameAgain)
